fd0470c90190d600f2526dead51993fed8c4bc94
Daniil Novoselov <gudnmail@gmail.com>
2021-07-25 12:49:56 +0500

    feat: add user dependends

---
+++ sdpremote/user.py
@@ -0,0 +1,44 @@
+import binascii
+from base64 import b64decode
+from typing import Optional
+
+from fastapi import Depends, Header, Query, status
+from fastapi.exceptions import HTTPException
+
+
+def _user_header(authorization: Optional[str] = Header(None)) -> str:
+    if not authorization:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+
+    splitted = authorization.split()
+    if len(splitted) != 2:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            'only support basic authz schema')
+
+    schema, value = splitted
+    if schema != 'Basic':
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            'only support basic authz schema')
+
+    try:
+        value = b64decode(value).decode()
+    except binascii.Error:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            'unable to decode value')
+
+    splitted = value.split(':')
+    if len(splitted) != 2:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, 'invalid value')
+
+    user = splitted[0]
+    if not user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, 'invalid value')
+
+    return user
+
+
+def user(user: Optional[str] = Query(None),
+         login: str = Depends(_user_header)) -> str:
+    if user is not None and user != login:
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+    return login
---
+++ tests/test_user.py
@@ -0,0 +1,63 @@
+from base64 import b64encode
+
+import pytest
+from fastapi import Depends, FastAPI, Query
+from fastapi.testclient import TestClient
+
+from sdpremote.user import user
+
+
+@pytest.fixture(scope='module')
+def app() -> FastAPI:
+    app = FastAPI()
+
+    def index(login: str = Depends(user)):
+        return login
+
+    def user_check(user: str = Query(...), _login: str = Depends(user)):
+        _login  # to disable lsp warning
+        return user
+
+    app.get('/')(index)
+    app.get('/{user}')(user_check)
+
+    return app
+
+
+@pytest.fixture(scope='module')
+def client(app: FastAPI) -> TestClient:
+    return TestClient(app)
+
+
+@pytest.fixture(scope='module')
+def headers() -> dict[str, str]:
+    value = b64encode(b'user:pass').decode()
+    return {'Authorization': f'Basic {value}'}
+
+
+def test_extract_user_from_header(client: TestClient, headers: dict[str, str]):
+    resp = client.get('/', headers=headers)
+    assert resp.status_code == 200
+    assert 'user' in resp.text
+
+
+def test_401_when_invalid_header(client: TestClient):
+    resp = client.get('/')
+    assert resp.status_code == 401
+
+    resp = client.get('/', headers={'Authorization': 'Digest blabla'})
+    assert resp.status_code == 401
+
+    resp = client.get('/', headers={'Authorization': 'Basic blabla'})
+    assert resp.status_code == 401
+
+
+def test_match_user_and_path(client: TestClient, headers: dict[str, str]):
+    resp = client.get('/user', headers=headers)
+    assert resp.status_code == 200
+    assert 'user' in resp.text
+
+
+def test_mismatch_user_and_path(client: TestClient, headers: dict[str, str]):
+    resp = client.get('/another', headers=headers)
+    assert resp.status_code == 403
