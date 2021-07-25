4260867b9feda97e6e3dcb3229edea0abab30fc3
Daniil Novoselov <gudnmail@gmail.com>
2021-07-25 11:59:36 +0500

    feat: add migrating system
    Use alembic

--- sdpremote/database.py
+++ sdpremote/database.py
@@ -1,6 +1,26 @@
+import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import create_async_engine  # type: ignore
 
 from .config import settings
 
-engine = create_async_engine(settings['database.uri'],
-                             echo=bool(settings['debug']))
+engine = create_async_engine(
+    settings['database.uri'],
+    echo=bool(settings['debug']),
+)
+
+convention = {
+    'all_column_names':
+    lambda consts, _: '_'.join(col.name for col in consts.columns.values()),
+    'ix':
+    'ix__%(table_name)s__%(all_column_names)s',
+    'uq':
+    'uq__%(table_name)s__%(all_column_names)s',
+    'ck':
+    'ck__%(table_name)s__%(constraint_name)s',
+    'fk':
+    'fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s',
+    'pk':
+    'pk__%(table_name)s'
+}
+
+metadata = sa.MetaData(naming_convention=convention)
