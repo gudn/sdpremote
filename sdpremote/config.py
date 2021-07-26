b5f0667234fc157463b097b9f55c21f7cd7eb394
Daniil Novoselov <gudnmail@gmail.com>
2021-07-26 14:35:51 +0500

    fix: remove port config param

--- sdpremote/config.py
+++ sdpremote/config.py
@@ -8,7 +8,6 @@ settings = Dynaconf(
         Validator('debug', default=False, is_type_of=bool),
         Validator('database.uri', must_exist=True, is_type_of=str),
         Validator('storage.endpoint', must_exist=True, is_type_of=str),
-        Validator('storage.port', must_exist=True, is_type_of=int),
         Validator('storage.secure', default=False, is_type_of=bool),
         Validator('storage.region', must_exist=True, is_type_of=str),
         Validator('storage.access_key', must_exist=True, is_type_of=str),
